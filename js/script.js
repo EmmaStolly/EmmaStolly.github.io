c8ee2dfd34867545fb7f46e28ed8ffdbe0bd78f5
EmmaStolly <33861433+EmmaStolly@users.noreply.github.com>
2018-03-14 13:47:53 +0000

    Third Commit

--- js/script.js
+++ js/script.js
@@ -19,13 +19,6 @@ navBrand.addEventListener('mouseenter', addBounce);
 //Listens to navBrand, when mouse leaves look at addbounce function.
 navBrand.addEventListener('animationend', removeBounce);
 
-//Variable looks for all navigation bar links
-//const navLinks = document.querySelectorAll('.nav-link');
-//Seperates the links so they don't all animate at the same time
-//navLinks.forEach(navLink => navLink.addEventListener('mouseenter', addBounce));
-//navLinks.forEach(navLink => navLink.addEventListener('animationend', removeBounce));
-
-
 //e = events
 function addBounce(e){
 //add animated bounce to navbar-brand class
@@ -36,4 +29,23 @@ function addBounce(e){
 function removeBounce(e){
 //remove animated bounce from navbar-brand class
 this.classList.remove('animated', 'bounce');
+}
+
+
+const hello = document.querySelector('.hello');
+//Listens to navBrand, when mouse over look at addbounce function.
+hello.addEventListener('mouseenter', addFont);
+//Listens to navBrand, when mouse leaves look at addbounce function.
+hello.addEventListener('animationend', removeFont);
+
+//e = events
+function addFont(e){
+//add animated bounce to navbar-brand class
+  this.classList.add('animated', 'fontalicious');
+}
+
+//e = events
+function removeFont(e){
+//remove animated bounce from navbar-brand class
+this.classList.remove('animated', 'fontalicious');
 }
