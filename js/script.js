c8f37d2f528b1ceae89fd0fb6ac3430d981e6643
EmmaStolly <33861433+EmmaStolly@users.noreply.github.com>
2018-03-14 22:05:22 +0000

    Fifth Commit

--- js/script.js
+++ js/script.js
@@ -1,17 +1,4 @@
-window.onload = function() {
-  currentYear();
-};
-
-
-function currentYear(){
-  const date = new Date();
-  const autoDate = document.querySelector('#autoDate');
-  autoDate.textContent = date.getFullYear();
-};
-
-
-//Making bounce animations on the nav bar
-
+//Makes 'Emma Stolly' on navbar bounce when hovered over
 //Variable that looks for anything in html with the class .navbar-brand
 const navBrand = document.querySelector('.navbar-brand');
 //Listens to navBrand, when mouse over look at addbounce function.
@@ -29,23 +16,4 @@ function addBounce(e){
 function removeBounce(e){
 //remove animated bounce from navbar-brand class
 this.classList.remove('animated', 'bounce');
-}
-
-
-const hello = document.querySelector('.hello');
-//Listens to navBrand, when mouse over look at addbounce function.
-hello.addEventListener('mouseenter', addFont);
-//Listens to navBrand, when mouse leaves look at addbounce function.
-hello.addEventListener('animationend', removeFont);
-
-//e = events
-function addFont(e){
-//add animated bounce to navbar-brand class
-  this.classList.add('animated', 'fontalicious');
-}
-
-//e = events
-function removeFont(e){
-//remove animated bounce from navbar-brand class
-this.classList.remove('animated', 'fontalicious');
 }
